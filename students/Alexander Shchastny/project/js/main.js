50428decbb809dff4577e2c9c14005b69cc91671
shchastny <aleksaemouse@gmail.com>
2019-11-23 22:19:59 +0300

    Сделал п.1 и п.3, с п.2 не понял что нужно сделать, пропустил
    п.3, дело в том, что в join не указали через что делать конкатенацию, по умолчанию по всей видимости это запятая.

--- students/Alexander Shchastny/project/js/main.js
+++ students/Alexander Shchastny/project/js/main.js
@@ -8,36 +8,36 @@ const ids = [1, 2, 3, 4, 5, 6, 7, 8];
 
 //глобальные сущности корзины и каталога (ИМИТАЦИЯ! НЕЛЬЗЯ ТАК ДЕЛАТЬ!)
 var userCart = [];
-var list = fetchData ();
+var list = fetchData();
 
 //кнопка скрытия и показа корзины
 document.querySelector('.btn-cart').addEventListener('click', () => {
     document.querySelector('.cart-block').classList.toggle('invisible');
 });
 //кнопки удаления товара (добавляется один раз)
-document.querySelector('.cart-block').addEventListener ('click', (evt) => {
-    if (evt.target.classList.contains ('del-btn')) {
-        removeProduct (evt.target);
+document.querySelector('.cart-block').addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('del-btn')) {
+        removeProduct(evt.target);
     }
 })
 //кнопки покупки товара (добавляется один раз)
-document.querySelector('.products').addEventListener ('click', (evt) => {
-    if (evt.target.classList.contains ('buy-btn')) {
-        addProduct (evt.target);
+document.querySelector('.products').addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('buy-btn')) {
+        addProduct(evt.target);
     }
 })
 
 //создание массива объектов - имитация загрузки данных с сервера
-function fetchData () {
+function fetchData() {
     let arr = [];
     for (let i = 0; i < items.length; i++) {
-        arr.push (createProduct (i));
+        arr.push(createProduct(i));
     }
     return arr
 };
 
 //создание товара
-function createProduct (i) {
+function createProduct(i) {
     return {
         id: ids[i],
         name: items[i],
@@ -59,58 +59,58 @@ function createProduct (i) {
                     </div>`
         },
 
-        add: function() {
+        add: function () {
             this.quantity++
         }
     }
 };
 
 //рендер списка товаров (каталога)
-function renderProducts () {
+function renderProducts() {
     let arr = [];
     for (item of list) {
         arr.push(item.createTemplate())
     }
-    document.querySelector('.products').innerHTML = arr.join();
+    document.querySelector('.products').innerHTML = arr.join('');
 }
 
-renderProducts ();
+renderProducts();
 
 //CART
 
 // Добавление продуктов в корзину
-function addProduct (product) {
+function addProduct(product) {
     let productId = +product.dataset['id'];
-    let find = userCart.find (element => element.id === productId);
+    let find = userCart.find(element => element.id === productId);
     if (!find) {
-        userCart.push ({
-            name: product.dataset ['name'],
+        userCart.push({
+            name: product.dataset['name'],
             id: productId,
             img: cartImage,
             price: +product.dataset['price'],
             quantity: 1
         })
-    }  else {
+    } else {
         find.quantity++
     }
-    renderCart ()
+    renderCart()
 }
 
 //удаление товаров
-function removeProduct (product) {
+function removeProduct(product) {
     let productId = +product.dataset['id'];
-    let find = userCart.find (element => element.id === productId);
+    let find = userCart.find(element => element.id === productId);
     if (find.quantity > 1) {
         find.quantity--;
     } else {
         userCart.splice(userCart.indexOf(find), 1);
         document.querySelector(`.cart-item[data-id="${productId}"]`).remove()
     }
-    renderCart ();
+    renderCart();
 }
 
 //перерендер корзины
-function renderCart () {
+function renderCart() {
     let allProducts = '';
     for (el of userCart) {
         allProducts += `<div class="cart-item" data-id="${el.id}">
