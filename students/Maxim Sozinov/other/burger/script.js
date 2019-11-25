f62d75caa06e133e3407fb5fb0b797c6ba551a9f
Maxim Sozinov <max.sozinov@gmail.com>
2019-11-25 16:13:46 +0700

    Burger finished

--- students/Maxim Sozinov/other/burger/script.js
+++ students/Maxim Sozinov/other/burger/script.js
@@ -1,63 +1,37 @@
-let btn = document.querySelector ('#makeBurger');
-let users = [];
+let btn = document.querySelector('#makeBurger');
+let burgers = [];
 
-btn.addEventListener ('click', addBurger);
+btn.addEventListener('click', addBurger);
 
-function addBurger () {
-    let newBurger = new Burger ("burgerSize", "burgerFilling", "burgerRelish");
-    console.log(newBurger);
+function addBurger() {
+    let newBurger = new Burger("burgerSize", "burgerFilling", "burgerRelish");
+    burgers.push(newBurger);
+    newBurger.show('calories');
+    newBurger.show('cost');
+    // console.log(newBurger);
 }
 
-// function addUser () {
-//     let newUser = new User ('Name', 'Age', 'Gender', 'Hobbies')
-//     users.push (newUser)
-// }
-
 class Burger {
-    constructor (size, fill, adds) {
+    constructor(size, fill, adds) {
         this.size = this._getProp(size);
         this.filling = this._getProp(fill);
         this.adds = this._getArr(adds);
-        this.calories = null;
-        this.cost = null;
+        this.calories = this._count("calories");
+        this.cost = this._count("price");
     }
-    _getProp (attrName) {
-        return document.querySelector (`input[name=${attrName}]:checked`).value;
+    _getProp(attrName) {
+        return document.querySelector(`input[name=${attrName}]:checked`).value;
     }
-    _getArr (attrName) {
-        let objArr = [...document.querySelectorAll (`input[name=${attrName}]:checked`)];
-        return objArr.map (el => el.value);       
+    _getArr(attrName) {
+        let objArr = [...document.querySelectorAll(`input[name=${attrName}]:checked`)];
+        return objArr.map(el => el.value);
+    }
+    _count(dataName) {
+        let objArr = [...document.querySelectorAll(`input:checked`)];
+        return objArr.reduce((acc, obj) => acc += +obj.dataset[dataName], 0);
+    }
+    show(prop) {
+        let obj = document.querySelector(`#${prop}`);
+        obj.innerHTML = `${this[prop]}`;
     }
 }
-
-
-// class User {
-//     constructor (name, age, gender, hobbies) {
-//         this.Name = this._text (name)
-//         this.Age = this._text (age)
-//         this.Gender = this._check (gender)
-//         this.Hobbies = this._getArray (hobbies)
-//         this.canBeMother = this._checkMom (gender)
-//     }
-//     _text (attrName) {
-//         let obj = document.querySelector (`input[name=${attrName}]`)
-//         return obj.value
-//     }
-//     _check (attrName) {
-//         let obj = document.querySelector (`input[name=${attrName}]:checked`)
-//         return obj.value
-//     }
-//     _getArray (attrName) {
-//         let objArr = [...document.querySelectorAll (`input[name=${attrName}]:checked`)]
-//         let arr = []
-//         objArr.forEach (el => {
-//             arr.push (el.value)
-//         })
-//         return arr
-//     }
-//     _checkMom (attrName) {
-//         let obj = document.querySelector (`input[name=${attrName}]:checked`)
-//         console.dir (obj)
-//         return +obj.dataset['mother'] ? true : false
-//     }
-// }
