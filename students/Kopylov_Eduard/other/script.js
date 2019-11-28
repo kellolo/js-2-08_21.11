ae30488d342c456e56e8f42d62f5bea1bc9ff05e
Eduard Kopylov <ed_010@mail.ru>
2019-11-28 14:16:16 +0300

    I did the second HM
    what kind of menu

--- students/Kopylov_Eduard/other/script.js
+++ students/Kopylov_Eduard/other/script.js
@@ -4,44 +4,72 @@ document.addEventListener("DOMContentLoaded",function()
     HumburgerCollection = [];
 
     
-    
+    //let oneForm = document.querySelector('#oneForm');
     let btn = document.querySelector('#clickbutton');
     btn.addEventListener("click",AddHumburger);
-    
-    
+    btn.addEventListener("click",InResult);
+    //btn.addEventListener("click",InResult2);
+
+    function InResult()
+    {
+        let obj = document.querySelector('.result');
+        for(let i=0; i <HumburgerCollection.length; i++)
+        {
+            obj.innerHTML = `Всего гамбургеров: ${HumburgerCollection.length} <br> Размер: ${HumburgerCollection[i].size}  <br>  Цена: ${HumburgerCollection[i].price} рублей <br>  Калории: ${HumburgerCollection[i].callories}`;
+        }
+        InResult2();
+       
+    }
+
+    function InResult2()
+    {
+        let obj = document.querySelector('.result-2');
+        let countPrice=0;
+        let countCal=0;
+        for(let i=0; i <HumburgerCollection.length; i++)
+        {
+            obj.innerHTML = `Итоговая Цена: ${countPrice += HumburgerCollection[i].price} рублей <br>  Калорий в сумме: ${ countCal += HumburgerCollection[i].callories}`;
+        }
+        
+       
+    }
+
+
     function AddHumburger()
     {
-        let newHb = new Humburger('Большой',10,20);
+        let newHb = new Humburger('Bg-type','Bg-filling','Bg-extra');
         HumburgerCollection.push(newHb);
     };
     
     class Humburger {
-        constructor(size,price, callories) { 
+        constructor(size,filling, extra) { 
     
             this.size = this._getSize(size);
-          //  this.stuffing = stuffing;
-            this.price = this._calculatePrice(price);
-            this.callories = this._calculateCalories(callories);
+            this.price = this._calculatePrice(size,filling, extra);
+            this.callories = this._calculateCalories(size,filling, extra);
          }
     
-        _addTopping(topping) {  }  // Добавить добавку }
-        _removeTopping(topping) {} // Убрать добавку }
-        _getToppings(topping) { }  // Получить список добавок }
+       
         _getSize(size) 
             { 
-                 let obj = document.querySelector(`input[name= ${size}]:checked`);
+                 let obj = document.querySelector(`input[name=${size}]:checked`);
                  return obj.dataset['size'];
                  
-            }        // Узнать размер гамбургера }
-        _getStuffing() {   }       // Узнать начинку гамбургера }
-        _calculatePrice(price) {
-            let obj = document.querySelector(`input[name= ${price}]:checked`);
-                 return +obj.dataset['price'];
-         }      // Узнать цену }
-        _calculateCalories(callories) {
-            let obj = document.querySelector(`input[name= ${callories}]:checked`);
-                 return +obj.dataset['callories'];
-         }   // Узнать калорийность }
+            }       
+       
+        _calculatePrice(size,filling,extra) {
+                let obj = document.querySelector(`input[name=${size}]:checked`);
+                let obj1 = document.querySelector(`input[name=${filling}]:checked`);
+                let obj2 = document.querySelector(`input[name=${extra}]:checked`);
+                 return +obj.dataset['price'] + +obj1.dataset['price'] + +obj2.dataset['price'];
+         }     
+
+        _calculateCalories(size,filling, extra) {
+            let obj = document.querySelector(`input[name=${size}]:checked`);
+            let obj1 = document.querySelector(`input[name=${filling}]:checked`);
+            let obj2 = document.querySelector(`input[name=${extra}]:checked`);
+             return +obj.dataset['callories'] + +obj1.dataset['callories'] + +obj2.dataset['callories'];
+         }  
       }
     
       
