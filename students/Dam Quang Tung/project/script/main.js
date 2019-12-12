26bf9f5d90786a5c76251b61f18a0ab556fd5619
Tung <damtung96@gmail.com>
2019-12-12 16:46:56 +0300

    Update json

--- students/Dam Quang Tung/project/script/main.js
+++ students/Dam Quang Tung/project/script/main.js
@@ -15,7 +15,6 @@ class List {
         this.container = container
         this.url = url
         this.items = []
-        //this.renderedItems = []
         this._init()
     }
     _init() {
@@ -30,16 +29,17 @@ class List {
             this.items.push(new lists[this.constructor.name](el))
         })
     }
-    _render() {
-        let bl = document.querySelector(this.container)
-        this.items.forEach(product => {
-            bl.insertAdjacentHTML('beforeend', product.render())
+    _render(arr = this.items) {
+        let el = document.querySelector(this.container)
+        el.innerHTML = ''
+        arr.forEach(product => {
+            el.insertAdjacentHTML('beforeend', product.render())
         })
     }
 }
 
 class Catalog extends List {
-    constructor(cart, url = API_URL + '/catalog.json', container = '.products') {
+    constructor(cart, url = `${API_URL}catalogData.json`, container = '.products') {
         super(url, container)
         this.cart = cart
     }
@@ -75,14 +75,17 @@ class Catalog extends List {
 }
 
 class Cart extends List {
-    constructor(url = API_URL + '/getBasket.json', container = '.cart-block') {
+    constructor(url = `${API_URL}getBasket.json`, container = '.cart-block', btnContainer = '.btn-cart') {
         super(url, container)
+        this.btnContainer = btnContainer
     }
     _init() {
         this.getJSON(this.url)
             .then(data => this.handleData(data.contents))
             .then(() => this._render())
+            .then(() => this._addEventListeners())
     }
+
     _addEventListeners() {
         document.querySelector(this.btnContainer).addEventListener('click', () => {
             document.querySelector(this.container).classList.toggle('invisible')
@@ -95,14 +98,14 @@ class Cart extends List {
     }
 
     _getCartItem(cartId) {
-        return this.items.find(cart => cart.id === cartId)
+        return this.items.find(cart => cart.id_product === cartId)
     }
 
     addCartItem(product) {
         this.getJSON(`${API_URL}addToBasket.json`)
             .then(data => {
                 if (data.result == 1) {
-                    let find = this._getCartItem(product.id)
+                    let find = this._getCartItem(product.id_product)
                     if (!find) {
                         this.items.push(new CartItem(product))
                     } else {
@@ -130,20 +133,20 @@ class Cart extends List {
 
 class Item {
     constructor(prod, img = image) {
-        this.id = prod.id
-        this.title = prod.title
+        this.id_product = prod.id_product
+        this.product_name = prod.product_name
         this.price = prod.price
         this.img = img
     }
     render() {
-        return `<div class="product-item" data-id="${this.id}">
+        return `<div class="product-item" data-id="${this.id_product}">
                     <img src="${this.img}" alt="Some img">
                     <div class="desc">
-                        <h3>${this.title}</h3>
+                        <h3>${this.product_name}</h3>
                         <p>${this.price} $</p>
                         <button class="buy-btn" 
-                        data-id="${this.id}"
-                        data-name="${this.title}"
+                        data-id="${this.id_product}"
+                        data-name="${this.product_name}"
                         data-image="${this.img}"
                         data-price="${this.price}">Купить</button>
                     </div>
@@ -156,21 +159,21 @@ class Product extends Item {}
 class CartItem extends Item {
     constructor(prod, img = cartImage) {
         super(prod, img)
-        this.quantity = prod.quantity
+        this.quantity = prod.quantity ? prod.quantity : 1
     }
     render() {
-        return `<div class="cart-item" data-id="${this.id}">
+        return `<div class="cart-item" data-id="${this.id_product}">
                     <div class="product-bio">
                         <img src="${this.img}" alt="Some image">
                         <div class="product-desc">
-                            <p class="product-title">${this.title}</p>
+                            <p class="product-title">${this.product_name}</p>
                             <p class="product-quantity">Quantity: ${this.quantity}</p>
                             <p class="product-single-price">$${this.price} each</p>
                         </div>
                     </div>
                     <div class="right-block">
                         <p class="product-price">${this.quantity * this.price}</p>
-                        <button class="del-btn" data-id="${this.id}">&times;</button>
+                        <button class="del-btn" data-id="${this.id_product}">&times;</button>
                     </div>
                 </div>`
     }
@@ -179,12 +182,10 @@ class CartItem extends Item {
 let lists = {
     Catalog: Product,
     Cart: CartItem
-    //Название класса списка: Класс соотв эл-та списка
 }
 
-let catalog = new Catalog()
 let cart = new Cart()
-
+let catalog = new Catalog(cart)
 
 
 //CART
