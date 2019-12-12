2b923ae2b3b94204dbe0e842fd46d968f8cb228b
kellolo <87kell@gmail.com>
2019-12-12 22:26:19 +0200

    6

---
+++ PROJECT/6 components/js/Catalog.js
@@ -0,0 +1,23 @@
+Vue.component ('catalog', {
+    data () {
+        return {
+            catalogUrl: '/catalogData.json',
+            imgCatalog: 'https://placehold.it/200x150',
+            items: []
+        }
+    },
+    methods: {
+        addProduct (product) {
+            console.log (`Куплен ${product.product_name}`)
+        }
+    },
+    mounted () {
+        this.$parent.getJson (this.catalogUrl)
+            .then (data => this.items = data)
+    },
+    template: `
+            <div class="products">
+                <catalog-item v-for="product of items" :item="product" :imgProp="imgCatalog"></catalog-item>
+            </div>
+    `
+})
