1105567f9b1b8350ead0f801b16f04cc05cfd3a2
havkin <max.sozinov@gmail.com>
2019-12-12 17:57:56 +0700

    added search

--- students/Maxim Sozinov/project/js/main.js
+++ students/Maxim Sozinov/project/js/main.js
@@ -1,79 +1,65 @@
+// const FAKE_API_CATALOG = 'https://raw.githubusercontent.com/havkin/js-2-08_21.11/master/students/Maxim%20Sozinov/fake-server/catalogData.json';
+// const FAKE_API_CART = 'https://raw.githubusercontent.com/havkin/js-2-08_21.11/master/students/Maxim%20Sozinov/fake-server/getBasket.json';
 
-//кнопка скрытия и показа корзины
-// document.querySelector('.btn-cart').addEventListener('click', () => {
-//     document.querySelector('.cart-block').classList.toggle('invisible');
-// });
-
-//кнопки удаления товара (добавляется один раз)
-// document.querySelector('.cart-block').addEventListener('click', (evt) => {
-//     if (evt.target.classList.contains('del-btn')) {
-//         userCart.removeItem(evt.target);
-//     }
-// });
-
-//кнопки покупки товара (добавляется один раз)
-// document.querySelector('.products').addEventListener('click', (evt) => {
-//     if (evt.target.classList.contains('buy-btn')) {
-//         userCart.addItem(evt.target);
-//     }
-// });
-
-const FAKE_API_CATALOG = 'https://raw.githubusercontent.com/havkin/js-2-08_21.11/master/students/Maxim%20Sozinov/fake-server/catalogData.json';
-const FAKE_API_CART = 'https://raw.githubusercontent.com/havkin/js-2-08_21.11/master/students/Maxim%20Sozinov/fake-server/getBasket.json';
-
-// const catalogContainer = '.products';
-const cartContainer = '.cart-block';
 
 class List {
-    constructor (url, container) {
+    constructor(url, container) {
         this.container = container;
         this.url = url;
         this.products = [];
-        this._init ();
+        this.filtered = [];
+        this._init();
     }
-    _init () {
+    _init() {
         return false;
     }
-    getJSON (url) {
-        return fetch (url)
-                .then (d => d.json ());
+    getJSON(url) {
+        return fetch(url)
+            .then(d => d.json());
     }
-    handleData (arr) {
+    handleData(arr) {
         arr.forEach(el => {
-            this.products.push (new lists[this.constructor.name] (el));
+            this.products.push(new lists[this.constructor.name](el));
+            this.filtered.push(new lists[this.constructor.name](el));
         });
     }
-    _render () {
-        let block = document.querySelector (this.container);
-        block.innerHTML ="";
-        this.products.forEach (item => {
-            block.insertAdjacentHTML ('beforeend', item.render ());
-        });
+    // _render() {
+    //     let block = document.querySelector(this.container);
+    //     block.innerHTML = "";
+    //     this.products.forEach(item => {
+    //         block.insertAdjacentHTML('beforeend', item.render());
+    //     });
+    // }
+    filterList(value) {
+        const regexp = new RegExp(value, 'i');
+        this.filtered = this.products.filter(good =>
+            regexp.test(good.product_name));
     }
 }
 
 class Catalog extends List {
-    constructor(url, container) {  // убрал cart из параметров
-        super (url, container);
+    constructor(url, container) { // убрал cart из параметров
+        super(url, container);
         // this.cart = cart   //-  вот это зачем?
     }
-    _init () {
-        this.getJSON (this.url)
-            .then (data => this.handleData (data));
-            // .then (() => this._render ());
+    _init() {
+        this.getJSON(this.url)
+            .then(data => this.handleData(data));
+        // .then (() => this._render ());
     }
+
 }
 
 class Cart extends List {
     constructor(url, container) {
-        super (url, container);
+        super(url, container);
         this.addItem_url = "https://raw.githubusercontent.com/havkin/js-2-08_21.11/master/students/Maxim%20Sozinov/fake-server/addToCart.json";
         this.removeItem_url = "https://raw.githubusercontent.com/havkin/js-2-08_21.11/master/students/Maxim%20Sozinov/fake-server/removeFromCart.json";
     }
-    _init () {
-        this.getJSON (this.url)
-            .then (data => this.handleData (data.contents));
-            // .then (() => this._render ());
+    _init() {
+        this.getJSON(this.url)
+            .then(data => this.handleData(data.contents));
+        // .then (() => this._render ());
     }
     addItem(item) {
         this._fetchData(this.addItem_url)
@@ -84,11 +70,11 @@ class Cart extends List {
                     if (!findItem) {
                         //заглушка для БД
                         let prod = {
-                            id : +item.dataset.id,
-                            title : item.dataset.name,
-                            price : +item.dataset.price,
-                            img : "https://placehold.it/100x80",  
-                            quantity : 1
+                            id: +item.dataset.id,
+                            title: item.dataset.name,
+                            price: +item.dataset.price,
+                            img: "https://placehold.it/100x80",
+                            quantity: 1
                         };
                         //                        
                         this.products.push(new CartItem(prod));
@@ -126,31 +112,31 @@ class Cart extends List {
             });
     }
     _fetchData(url) {
-        return fetch(url) .then(dataJSON => dataJSON.json());
+        return fetch(url).then(dataJSON => dataJSON.json());
     }
 }
 
 class Item {
-    constructor (prod) {
+    constructor(prod) {
         this.id_product = prod.id;
         this.product_name = prod.title;
         this.price = prod.price;
         this.img = prod.img;
     }
-    render () {
-        return `<div class="product-item" data-id="${this.id_product}">
-                    <img src="${this.img}" alt="Some img">
-                    <div class="desc">
-                        <h3>${this.product_name}</h3>
-                        <p>${this.price} $</p>
-                        <button class="buy-btn" 
-                        data-id="${this.id_product}"
-                        data-name="${this.product_name}"
-                        data-image="${this.img}"
-                        data-price="${this.price}">Купить</button>
-                    </div>
-                </div>`;
-    }
+    // render () {
+    //     return `<div class="product-item" data-id="${this.id_product}">
+    //                 <img src="${this.img}" alt="Some img">
+    //                 <div class="desc">
+    //                     <h3>${this.product_name}</h3>
+    //                     <p>${this.price} $</p>
+    //                     <button class="buy-btn" 
+    //                     data-id="${this.id_product}"
+    //                     data-name="${this.product_name}"
+    //                     data-image="${this.img}"
+    //                     data-price="${this.price}">Купить</button>
+    //                 </div>
+    //             </div>`;
+    // }
 }
 
 class Product extends Item {}
@@ -160,22 +146,22 @@ class CartItem extends Item {
         super(product);
         this.quantity = product.quantity;
     }
-    render () {
-        return `<div class="cart-item" data-id="${this.id_product}">
-                    <div class="product-bio">
-                        <img src="${this.img}" alt="Some image">
-                        <div class="product-desc">
-                            <p class="product-title">${this.product_name}</p>
-                            <p class="product-quantity">Quantity: ${this.quantity}</p>
-                            <p class="product-single-price">$${this.price} each</p>
-                        </div>
-                    </div>
-                    <div class="right-block">
-                        <p class="product-price">${this.quantity * this.price}</p>
-                        <button class="del-btn" data-id="${this.id_product}">&times;</button>
-                    </div>
-                </div>`;
-    }
+    // render () {
+    //     return `<div class="cart-item" data-id="${this.id_product}">
+    //                 <div class="product-bio">
+    //                     <img src="${this.img}" alt="Some image">
+    //                     <div class="product-desc">
+    //                         <p class="product-title">${this.product_name}</p>
+    //                         <p class="product-quantity">Quantity: ${this.quantity}</p>
+    //                         <p class="product-single-price">$${this.price} each</p>
+    //                     </div>
+    //                 </div>
+    //                 <div class="right-block">
+    //                     <p class="product-price">${this.quantity * this.price}</p>
+    //                     <button class="del-btn" data-id="${this.id_product}">&times;</button>
+    //                 </div>
+    //             </div>`;
+    // }
     increaseQnt() {
         this.quantity++;
     }
@@ -191,56 +177,56 @@ let lists = {
 
 };
 
-let catalog = new Vue ({
+let vm_catalog = new Vue({
     el: '#catalog',
     data: {
         catalog: {},
-        catalogContainer: '.products'
+        catalogContainer: '.products',
+        FAKE_API_CATALOG: 'https://raw.githubusercontent.com/havkin/js-2-08_21.11/master/students/Maxim%20Sozinov/fake-server/catalogData.json'
     },
     methods: {
-        addInCart (evt) {
-            cart.cart.addItem(evt.target);
+        addInCart(evt) {
+            vm_cart.cart.addItem(evt.target);
         }
     },
-    mounted () {
-        this.catalog = new Catalog (FAKE_API_CATALOG, this.catalogContainer);
+    mounted() {
+        this.catalog = new Catalog(this.FAKE_API_CATALOG, this.catalogContainer);
         console.log(this.catalog);
     }
 });
 
-let cart = new Vue ({
+let vm_cart = new Vue({
     el: '#cart',
     data: {
-        show: true,
-        cart: {}
+        show: false,
+        cart: {},
+        cartContainer: '.cart-block',
+        FAKE_API_CART: 'https://raw.githubusercontent.com/havkin/js-2-08_21.11/master/students/Maxim%20Sozinov/fake-server/getBasket.json'
     },
     methods: {
-        toggleShow () {
+        toggleShow() {
             this.show = !this.show;
         },
-        removeItem (evt) {
+        removeItem(evt) {
             this.cart.removeItem(evt.target);
         }
     },
-    computed: {
-
-    },
-    mounted () {
-        this.cart = new Cart (FAKE_API_CART, cartContainer);
+    mounted() {
+        this.cart = new Cart(this.FAKE_API_CART, this.cartContainer);
         console.log(this.cart);
     }
 
 });
 
-// main
-// ------------------------------------------------
-
-// let catalog = new Catalog(FAKE_API_CATALOG, catalogContainer);
-// let userCart = new Cart(FAKE_API_CART, cartContainer);
-
-//кнопки удаления товара (добавляется один раз)
-// document.querySelector('.cart-block').addEventListener('click', (evt) => {
-//     if (evt.target.classList.contains('del-btn')) {
-//         userCart.removeItem(evt.target);
-//     }
-// });
+let vm_search = new Vue({
+    el: '#search',
+    data: {
+        value: '',
+        filtered: []
+    },
+    methods: {
+        filterGoods() {
+            vm_catalog.catalog.filterList(this.value);
+        }
+    }
+});
