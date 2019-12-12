4e5e20c63961eb6541d1d4b418a2fb854cd02f2a
Andrey <andrey.o@list.ru>
2019-12-12 05:34:27 +0500

    start webpack configure

---
+++ students/Andrey Osipuk/project/webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin'); // плагин для загрузки кода Vue
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      components: path.resolve(__dirname, 'src', 'components'),
+      assets: path.resolve(__dirname, 'src', 'assets'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }, {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+}
