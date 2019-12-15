ee59282224d243f26fa6a78917c29ec909f717a7
Konstantin-Buzuev <55285803+Konstantin-Buzuev@users.noreply.github.com>
2019-12-15 23:12:30 +0400

    HomeWork 6

--- students/Konstantin Buzuev/project/js/app.js
+++ students/Konstantin Buzuev/project/js/app.js
@@ -3,11 +3,7 @@ const app = new Vue({
     el: "#app",
     data: {
         catalogURL: "https://raw.githubusercontent.com/Konstantin-Buzuev/online-store-api/master/OnlineShop/catalog.json",
-        imgURL: "img/",
-        /*Прикрутить к проекту загрузку картинок с сервака!*/
-        /*'https: //github.com/Konstantin-Buzuev/online-store-api/tree/master/OnlineShop/img/',*/
         products: [],
-        searchString: ""
     },
     methods: {
         /*Получение данных с API*/
@@ -18,7 +14,6 @@ const app = new Vue({
         parseProducts() {
             this.products.forEach(product => {
                 this._parseDescription(product);
-                this._parseImage(product);
                 Vue.set(product, "hidden", false)
             });
         },
@@ -28,22 +23,6 @@ const app = new Vue({
             let details = descr.slice(descr.indexOf(".", 50) + 2); // Пропускаем пробел после точки
             Vue.set(product, "summary", summary);
             Vue.set(product, "details", details);
-        },
-        _parseImage(product) {
-            Vue.set(
-                product,
-                "imageURL",
-                this.imgURL + "product-" + product.id + ".jpg"
-            );
-            Vue.set(
-                product,
-                "fullImageURL",
-                this.imgURL + "product-" + product.id + "-full" + ".jpg"
-            );
-        },
-        /*Вынести в отдельный компонент!*/
-        filterCatalog() {
-            eventBus.$emit("search", this.searchString)
         }
     },
     mounted() {
